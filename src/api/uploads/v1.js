61dff2bb5d9c028fe491c823da02df58900af260
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-19 19:07:21 +0700

    feat(uploads): :sparkles: Update uploads in the file v1.js

--- src/api/uploads/v1.js
+++ src/api/uploads/v1.js
@@ -26,7 +26,32 @@ const uploadsProfileAPI = async (file) => {
     }
 }
 
+const uploadsThumbnailAPI = async (file) => {
+    // Create a FormData object to send the file
+    const formData = new FormData()
+    formData.append("thumbnail", file)
+
+    try {
+        // Replace with your actual API endpoint for file upload
+        const axiosResponse = await axiosInstance.post("/uploads/thumbnail", formData, {
+            headers: { "Content-Type": "multipart/form-data", "Accept": "application/json" },
+        })
+
+        const body = axiosResponse.data;
+        const data = body.data;
+        return data;
+    } catch (error) {
+        console.error("Error uploading photo:", error);
+        if (error instanceof AxiosError) {
+            throw new Error(axiosErrorResponseToMessage(error));
+        }
+
+        throw new Error("Failed to upload photo. Please try again.")
+    }
+}
+
 
 export {
     uploadsProfileAPI,
+    uploadsThumbnailAPI,
 }
