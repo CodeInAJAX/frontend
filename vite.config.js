950de82b71b2a175251c34b5a2980d3108fe2bc7
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-28 14:39:20 +0700

    chore: Update vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [react(),tailwindcss(),basicSsl()],
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),basicSsl()],
+  // plugins: [react(),tailwindcss()],
   server: {
     https: true,
     proxy: {
