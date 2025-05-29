f083aae86a1a86dedfa07c7182551cceb19ec709
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-29 13:16:30 +0700

    chore: Update users.js files

--- src/validation/users.js
+++ src/validation/users.js
@@ -12,7 +12,7 @@ export const genderUser = {
 };
 
 export const registerSchema = z.object({
-    nama: z.string()
+    name: z.string()
         .min(2, { message: "Nama minimal 2 karakter" })
         .nonempty({ message: "Nama wajib diisi" }),
 
@@ -29,7 +29,7 @@ export const registerSchema = z.object({
         ),
 
     role: z.nativeEnum(roleUser, {
-        errorMap: () => ({ message: "Peran tidak valid. Pilih antara admin, guru, atau siswa" })
+        errorMap: () => ({ message: "Peran tidak valid. Pilih antara guru, atau siswa" })
     }),
 
     gender: z.nativeEnum(genderUser, {
@@ -58,3 +58,38 @@ export const loginSchema = z.object({
         message: "Konfirmasi Password harus sama dengan Password",
         path: ["confirmPassword"],
     });
+
+
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+
+export const editUserSchema = z.object({
+    name: z.string()
+        .min(2, { message: "Nama minimal 2 karakter" })
+        .nonempty({ message: "Nama wajib diisi" }),
+
+    password: z.string()
+        .nonempty({ message: "Password wajib diisi" })
+        .min(8, { message: "Password minimal 8 karakter" })
+        .refine(
+            (password) => /(?=.*[a-zA-Z])(?=.*[0-9])/.test(password),
+            { message: "Password harus berisi huruf dan angka" }
+        ),
+
+    gender: z.nativeEnum(genderUser, {
+        errorMap: () => ({ message: "Jenis kelamin tidak valid. Pilih antara laki - laki atau perempuan" })
+    }),
+
+    photo: z
+        .custom((file) => {
+        if (!file) return true; // optional
+        return file instanceof File &&
+            ACCEPTED_IMAGE_TYPES.includes(file.type) &&
+            file.size <= MAX_FILE_SIZE;
+    }, {
+        message: "Foto harus berupa gambar JPG, JPEG, atau PNG dan maksimal berukuran 2MB"
+    }),
+    about: z.string()
+        .min(5, { message: "Tentang minimal 5 karakter" })
+        .default("Tidak ada deskripsi")
+});
