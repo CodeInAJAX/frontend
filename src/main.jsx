a40d9ce4b5bb7a8004692de9fd2db17ad232203c
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-02 08:31:48 +0700

    feat(src): :sparkles: Update src in the file main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router"
 import { router } from "./routers"
-import { AuthProvider } from "./context/authContext"
+import { AppProvider } from "./context/appContext.jsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
+    <AppProvider>
       <RouterProvider router={router} />
-    </AuthProvider>
+    </AppProvider>
   </StrictMode>,
 )
