fd3cb9e0ab50500e5fd52eba905c9dc11fdb25f5
tirtahakimpambudhi <awebthp@gmail.com>
2025-06-11 08:31:59 +0700

    feat(validation): :sparkles: Update validation in the file users.js

--- src/validation/users.js
+++ src/validation/users.js
@@ -11,7 +11,7 @@ export const genderUser = {
   FEMALE: "female",
 };
 
-export const register = z.object({
+export const registerSchema = z.object({
     nama: z.string()
         .min(2, { message: "Nama minimal 2 karakter" })
         .nonempty({ message: "Nama wajib diisi" }),
@@ -35,5 +35,26 @@ export const register = z.object({
     gender: z.nativeEnum(genderUser, {
         errorMap: () => ({ message: "Jenis kelamin tidak valid. Pilih antara laki - laki atau perempuan" })
     }),
-    photo: z.any().refine((val) => val !== null, "Foto profil diperlukan"),
 });
+
+export const loginSchema = z.object({
+    email: z.string()
+        .nonempty({ message: "Email wajib diisi" })
+        .email({ message: "Format email tidak valid" }),
+
+    password: z.string()
+        .nonempty({ message: "Password wajib diisi" })
+        .min(8, { message: "Password minimal 8 karakter" })
+        .refine(
+            (password) => /(?=.*[a-zA-Z])(?=.*[0-9])/.test(password),
+            { message: "Password harus berisi huruf dan angka" }
+        ),
+
+    confirmPassword: z.string()
+        .nonempty({ message: "Konfirmasi Password wajib diisi" })
+        .min(8, { message: "Password minimal 8 karakter" })
+})
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Konfirmasi Password harus sama dengan Password",
+        path: ["confirmPassword"],
+    });
